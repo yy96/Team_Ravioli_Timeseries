efc9b5893f4262a1408add4bb6c23060faa72ce6
yidi <yidi.hu@epfl.ch>
2020-04-10 05:34:18 +0800

    updated exponential smoothing

---
+++ ExpoSmoothing_clustering.py
@@ -0,0 +1,248 @@
+#========== Loading packages ==========# 
+import quantiacsToolbox as qt
+import numpy as np
+from statsmodels.tsa.api import ExponentialSmoothing as eps
+from sklearn.cluster import AgglomerativeClustering as agg
+from scipy.spatial.distance import cosine
+from itertools import groupby
+from helper import eval_exponential, validate, check, read_saved_data, read_setup, save_initial, save_daily, save_output
+import warnings,os
+warnings.filterwarnings("ignore")
+
+
+
+#========== HW parameters ==========#
+markets = ['F_AD','F_BO','F_BP','F_C','F_CC','F_CD','F_CL','F_CT',
+           'F_DX','F_EC','F_ED','F_ES','F_FC','F_FV','F_GC','F_HG',
+           'F_HO','F_JY','F_KC','F_LB','F_LC','F_LN','F_MD','F_MP',
+           'F_NG','F_NQ','F_NR','F_O','F_OJ','F_PA','F_PL','F_RB',
+           'F_RU','F_S','F_SB','F_SF','F_SI','F_SM','F_TU','F_TY',
+           'F_US','F_W','F_XX','F_YM','F_AX','F_CA','F_DT','F_UB',
+           'F_UZ','F_GS','F_LX','F_SS','F_DL','F_ZQ','F_VX','F_AE',
+           'F_BG','F_BC','F_LU','F_DM','F_AH','F_CF','F_DZ','F_FB',
+           'F_FL','F_FM','F_FP','F_FY','F_GX','F_HP','F_LR','F_LQ',
+           'F_ND','F_NY','F_PQ','F_RR','F_RF','F_RP','F_RY','F_SH',
+           'F_SX','F_TR','F_EB','F_VF','F_VT','F_VW','F_GD','F_F']
+
+start,end = '20180119','20200331'#'20171030', '20191231'#
+threshold = 0.6
+interval = 10 ### interval between finding a new look-back and fitting a new model for each market
+period = 5
+test_period = interval
+path = save_output(interval,period,threshold)
+
+
+
+#========== Cluster parameters ==========#
+smooth_alpha=5
+num_cluster= 3
+cluster_period=500
+cluster_on = True
+dist_func = cosine
+
+
+
+#========== Helper functions: Dynamic lookback ==========#
+def set_train_period(path, CLOSE, t_dict, a_dict):
+    """
+    helper function that is called only once at the start to search for lookback
+    period that has highest accuracy in predicting direction of changefor each market.
+    In the subsequent days, call adjust_period() instead to tune the lookback period based on previous lookback period
+    """
+    if "periods.txt" in os.listdir(path):
+        return read_setup(path)
+    
+    nMarkets=CLOSE.shape[1]
+    for future_id in range(1, nMarkets):
+        max_acc, train_period = 0, 0
+        cur = np.array([k for k,g in groupby(CLOSE[:,future_id])])
+        for i in range(50,501,50):
+            accuracy, temp_model = check(cur, i,period, test_period)
+            if accuracy > max_acc:
+                max_acc = accuracy
+                train_period = i
+        if max_acc==0:train_period=0
+        print('{}:({},{}%)'.format(markets[future_id-1], train_period, int(max_acc*100)))    
+        t_dict[future_id] = train_period
+        a_dict[future_id] = max_acc
+    save_initial(path, t_dict, a_dict)
+    return t_dict, a_dict
+
+
+def adjust_period(full_data, prev_period):
+    """
+    helper function to adjust the lookback period slightly around its previous lookback period
+    called everytime the model is retrained 
+    """
+    
+    if not prev_period: prev_period=100
+    max_acc, best_period, model = 0, prev_period, None
+
+
+    for i in [-30,0,30]:
+        new_period = prev_period+i
+        if new_period<50 or new_period>500: new_period=250
+
+        try:
+            accuracy, temp_model = check(full_data, new_period, period,test_period)
+        except:
+            continue
+        
+        if accuracy>max_acc:
+            best_period = new_period
+            model = temp_model
+            max_acc = accuracy
+
+    print("{}-->{}".format(prev_period,best_period, period))
+    return best_period, model, round(max_acc,2)
+
+
+
+#========== Helper functions: Clustering ==========#
+def smooth(M, interval):
+    smoothed = np.mean(M[:interval,],axis=0)
+    for i in range(1, int(M.shape[0]/interval)):
+        start = i*interval
+        new = np.mean(M[start:start+interval,],axis=0)
+        smoothed = np.vstack((smoothed,new))
+    return smoothed
+
+def normalize(included_M):
+    m = np.mean(included_M, axis=0)
+    se = np.std(included_M, axis=0)
+    return (included_M-m)/se
+
+def cluster(M, num_cluster, acc_list, dist_func, smooth_alpha):
+    M = normalize(smooth(M, smooth_alpha))
+    D = compute_dist_matrix(M, dist_func)
+    clusters = agg(n_clusters=num_cluster, affinity='precomputed', linkage = 'average').fit_predict(D)
+    weightage = np.ones(num_cluster)
+    for i in range(num_cluster):
+        weightage[i] = np.mean(acc_list[clusters==i])
+    weightage = weightage/np.sum(weightage)
+    return clusters, weightage
+
+def compute_dist_matrix(M, dist_func):
+    n_markets =  M.shape[1]
+    dist_mat = np.zeros((n_markets,n_markets))
+    for i in range(n_markets):
+        for j in range(i+1, n_markets):
+            dist = dist_func(M[:,i], M[:,j])
+            dist_mat[i,j], dist_mat[j,i] = dist, dist
+    return dist_mat
+
+
+def group_norm(num_cluster, included, cluster, weights, show,weightage):
+    included = np.array(included) ##1-89
+    for i in range(num_cluster):
+        w = weightage[i]
+        indices = included[cluster==i]
+        cur_cluster = weights[indices]
+        norm_cluster = (cur_cluster*w)/(np.nansum(np.abs(cur_cluster)))
+        weights[indices] = norm_cluster
+        names = np.array(markets)[indices-1]
+        if show: print("\ncluster {}({}): {}\n".format(i, round(weightage[i],2),names))
+    weightage = weightage/np.sum(weightage)
+    return weights
+
+
+
+#========== Trading System ==========#
+def myTradingSystem(DATE, CLOSE, settings):
+    ### 0.SET UP
+    # num and build: counter used to retrain the model by interval
+    # settings['train_period']: record individual market's lookback period found in the last retrain: at the start
+    # settings['accuracies']: record the accuracy achieved in the last retain using the recorded lookback period and test period for each market
+    # filtered: counter of how many markets are removed due to inaccurate model or non-convergent problem
+    num = settings['counter']%interval
+    build = num==0
+    nMarkets=CLOSE.shape[1]
+    weights = np.zeros(nMarkets)
+    filtered=0
+    date = DATE[-1]
+    settings['counter']+=1
+    if settings['counter']==1:
+        settings['train_period'], settings['accuracies'] = set_train_period(path, CLOSE, settings['train_period'], 
+                                                                            settings['accuracies'])
+##    weights,acc = read_saved_data(path,date)
+##    if weights:
+##        return saved_results[0], saved_results[1]
+    
+
+    ### 1. Data preprocessing: from raw data to input 
+    # CLOSE: log the prices to 1)linearize the trend 2)reduce the impact of different price scale of different markets
+    # CLOSE: remove consecutive duplicates as that will cause the model to produce nan values when it detects constant pattern
+    print('\n{}'.format(date))
+    for i in range(1,nMarkets):        
+        cur = np.log(np.array([k for k,g in groupby(CLOSE[:,i])]))
+        pred, model=None, None
+
+        ### 2. Model building: process input to produce predictions
+        # prev_period: the last lookback period used for this market in the last retrain; might be ajusted slightly if another number can achieve better performance
+        # train_period, model, accuracy: the adjusted lookback period, model trained using the new period and the accuracy for the retrained model in the validation data
+        if build:
+            prev_period = settings['train_period'][i]
+            train_period, model, accuracy = adjust_period(cur, prev_period)
+
+            ### 3. Model filtering
+            # threshold: if the accuracy is less than the threshold, exclude the market in the final output to avoid loss
+            # settings['models']: if the model achieves acceptable accuracy, save it for next day prediction until next retrain
+            if accuracy<threshold:
+                model=None
+                print(' {}%==={}: inaccurate'.format(int(100*accuracy), settings['markets'][i]))
+            settings['models'][i] = model
+            settings['train_period'][i] = train_period
+            settings['accuracies'][i] = accuracy
+        else:
+            model = settings['models'][i]
+            accuracy = settings['models'][i]
+            accuracy= settings['accuracies'][i]
+                    
+        if model:
+            ### 4. Prediction and equity allocation
+            # pred: the HW model will predict the next log(price)
+            # weights: take the predicted log(price) and minus the latest observed log(price) as the final output weights
+            pred = validate(cur[-1],model.forecast(num+1)[-1])
+            if not np.isnan(pred):
+                print(' {}%==={}'.format(int(accuracy*100), settings['markets'][i]))
+                weights[i] = pred-cur[-1]
+            else: 
+                print(' {}%==={}: non-convergent'.format(int(accuracy*100), settings['markets'][i]))
+
+    acc = settings['accuracies']
+    included = np.nonzero(weights)[0]        
+    save_daily(path, date, weights, acc)
+
+    if cluster_on:
+        if build: ##update value
+            clusters, weightage = cluster(CLOSE[-cluster_period:,included], num_cluster, np.array(acc)[included], dist_func, smooth_alpha)
+            settings['clusters']=clusters 
+            settings['weightage']=weightage
+            settings['included']=included
+        else: ## use old values
+            clusters = settings['clusters']
+            weightage = settings['weightage']
+            included=settings['included']
+        weights = group_norm(num_cluster, included, clusters, np.array(weights), build, weightage)           
+        
+    return weights, settings
+
+
+def mySettings():
+    settings= {}
+    settings['markets']  = ['CASH']+markets
+    settings['beginInSample'] = start
+    settings['endInSample'] = end
+    settings['lookback']= 504
+    settings['budget']= 10**6
+    settings['slippage']= 0.05
+    settings['models'] = [None]*len(settings['markets'])
+    settings['train_period'] = [0]*len(settings['markets'])
+    settings['accuracies'] = [0]*len(settings['markets'])
+    settings['num_trades']=[]
+    settings['counter']=0
+    return settings
+
+if __name__ == '__main__':
+    import quantiacsToolbox
+    results = quantiacsToolbox.runts(__file__)
--- helper.py
+++ helper.py
@@ -1,10 +1,58 @@
 import numpy as np
 from pmdarima import auto_arima
 from statsmodels.tsa.api import ExponentialSmoothing as eps
+import os
 
+
+
+#========== 1. HW helper functions ==========#
+def eval_exponential(curr_market, train_period, test_period, period):
+    acc_list = []
+    for i in range(3):
+        last_train = i+train_period
+        train = curr_market[i:last_train]
+        test = curr_market[last_train-1:last_train+test_period]
+        pred,model = eps_predict(train,period,len(test))
+        acc_list.append(eval_model(pred, test))
+    return sum(acc_list)/len(acc_list)
+
+
+def eps_predict(train,period,num_forecasts):
+    model = eps(train, seasonal_periods=period, trend="add",seasonal="add").fit(use_boxcox=True)
+    pred = model.forecast(num_forecasts)
+    pred = validate(train[-1],pred)
+    return pred,model
+
+
+def validate(obs,pred):
+    validate = abs(obs/pred)
+    x = np.logical_or(validate < 0.5, validate > 1.5).astype(int)
+    y = x*(validate.astype(int)-1)
+    return pred+y
+
+
+def check(data, lookback,period,test_period):
+    """
+    helper function called be set_train_period() and adjust_period()
+    @params: data: the set of datapoints to be modelled
+             lookback: the number of datapoints as training data to do multi-step ahead forecasts
+    """
+    model = eps(data[-lookback:], seasonal_periods=period, trend="add", seasonal="add").fit(use_boxcox=True)
+    accuracy=0
+    if np.mean(np.isnan(model.forecast(test_period).astype(int)))<0.5:
+        train = data[-lookback-test_period-3:]
+        accuracy = eval_exponential(train, lookback, test_period, max(int(lookback/30),3))
+    return accuracy, model
+
+
+
+#========== 2. ARIMA helper functions ==========#
 # evaluate the performace of the model based on price signal
 def eval_model(predicted, observed):
-    return np.mean((predicted*observed >0).astype(int))
+    pred = np.diff(predicted)
+    obs = np.diff(observed)
+    return np.mean((pred*obs>0).astype(int))
+
 
 def eval_autoarima(train, test):
     # sarima_train_model = auto_arima(curr_market, error_action='ignore', suppress_warnings=True, seasonal=True, m=4)
@@ -14,47 +62,82 @@ def eval_autoarima(train, test):
     arima_train_model.fit(train)
     arima_predicted = np.diff(arima_train_model.predict(n_periods=20))
     arima_accuracy = eval_model(arima_predicted, np.diff(test))
-
     return arima_accuracy
 
-def eval_exponential(curr_market, train_period, test_period, period):
-    acc_list = []
-    for i in range(3):
-        last_train = i+train_period
-        train = curr_market[i:last_train]
-        test = curr_market[last_train:last_train+test_period]
-        model = eps(train, seasonal_periods=period, trend="add",seasonal="add").fit(use_boxcox=True)
-        pred = np.diff(model.forecast(len(test)))
-        acc_list.append(eval_model(pred, np.diff(test)))
-    return sum(acc_list)/len(acc_list)
 
 def pred_autoarima(arima_model, curr_market, market_name):
     # sarima_model = auto_arima(curr_market, error_action='ignore', suppress_warnings=True, seasonal=True, m=4)
     arima_model.fit(curr_market)
     print('==={}:{} {}'.format(market_name, arima_model.params(), arima_model.order))
     arima_pred = arima_model.predict(n_periods=1)[0]
-
     return arima_pred
 
-def pred_exponential(curr_market, market_name):
-    exponential_model = eps(curr_market, seasonal_periods=12, trend='add', seasonal='add').fit(use_boxcox=True)
-    exponential_pred = exponential_model.forecast(1)[0]
-    a, b, c = exponential_model.params['smoothing_level'], exponential_model.params['smoothing_slope'], \
-              exponential_model.params['smoothing_seasonal']
-    print('==={}:{} {} {}'.format(market_name, round(a, 2), round(b, 2), round(c, 2)))
-    return exponential_pred
 
+
+#==========3. Ensemble helper functions ==========#
 def simple_ensemble(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
     techanalysis_output, _ = techanalysis(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings)
     arima_output, settings = autoarima(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings)
 
     #weighted_arima = [float(i)/sum(arima_output) for i in arima_output]
     #weighted_techanalysis = [float(i)/sum(techanalysis_output) for i in techanalysis_output]
-
-
     weights = np.array([a+b for a,b in zip(arima_output, techanalysis_output)])
     print (techanalysis_output)
     print (weights)
     #weights = [float(i)/sum(weights) for i in weights]
-
     return weights, settings
+
+
+
+#========== # utility functions ==========#
+def read_saved_data(path,date):
+    if "{}.txt".format(date) in os.listdir(path):
+        weights,acc = read_daily(path,date)
+        return np.array(weights),acc
+    return None
+
+
+def save_output(x,y,z):
+    cwd = os.getcwd()
+    name = "{}_{}_{}".format(x,y,z)
+    path = os.path.join(cwd, name)
+    if name not in os.listdir(cwd):
+        os.mkdir(path)
+    return path
+
+
+def save_initial(path, t_dict,a_dict):
+    t_file=open(path+'/periods.txt','w')
+    a_file=open(path+'/accuracies.txt','w')
+    for t in t_dict:
+         t_file.write("{}\n".format(str(t)))
+    t_file.close()
+
+    for a in a_dict:
+         a_file.write("{}\n".format(str(a)))
+    a_file.close()
+
+
+def save_daily(path,date,weights,acc):
+    file=open(path+'/{}.txt'.format(date),'w')
+    for i in range(len(weights)):
+         file.write("{},{}\n".format(str(weights[i]),str(acc[i])))
+    file.close()
+
+
+def read_daily(path,date):
+    t=open(path+'/{}.txt'.format(date,'r')).readlines()
+    weights, acc=[],[]
+    for each in t:
+        out = each.split(',')
+        weights.append(float(out[0]))
+        acc.append(float(out[1]))
+    return weights,acc
+
+
+def read_setup(path):
+    t=open(path+'/periods.txt','r').readlines()
+    a=open(path+'/accuracies.txt','r').readlines()
+    t_dict = [int(i) for i in t]
+    a_dict = [float(i) for i in a]
+    return t_dict,a_dict
